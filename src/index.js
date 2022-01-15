1b7be3e909bc830803918936481c7886178a67c6
Akalax <93571768+Akalax@users.noreply.github.com>
2022-01-15 14:08:06 +0530

    FinalSubmission

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
 const route = require('./routes/route.js');
 
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require('mongoose');
-const { response } = require('express');
 
 mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/<group_15>?retryWrites=true&w=majority")
     .then(() => console.log('mongodb running on 27017'))
